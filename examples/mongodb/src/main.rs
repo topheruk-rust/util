4e5536973024378417273aff6adc11810e617fc2
topheruk <kristopherab@gmail.com>
2022-03-01 20:36:34 +0000

    .

--- examples/mongodb/src/main.rs
+++ examples/mongodb/src/main.rs
@@ -11,19 +11,19 @@ use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Load the MongoDB connection string from an environment variable:
     let client_uri =
          "mongodb+srv://topheruk:VsNSZ28UcbGYJhw2@cluster0.pkfdw.mongodb.net/local?retryWrites=true&w=majority";
+    // no idea how to load env on a linux device using workspaces, but will learn
     // env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
 
-    // A Client is needed to connect to MongoDB:
+    // -- connect to client
     // An extra line of code to work around a DNS issue on Windows:
     let options =
         ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
             .await?;
     let client = Client::with_options(options)?;
 
-    // -- print name sof all databases
+    // -- print names of all databases in cluster that client is connected to
     println!("Databases:");
     for name in client.list_database_names(None, None).await? {
         println!("- {}", name);
@@ -33,30 +33,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
     use chrono::{TimeZone, Utc};
     use mongodb::bson::{self, doc, Bson};
 
-    let parasite = Movie {
-        id: None,
-        title: "Parasite".to_string(),
-        year: 2020,
-        plot: "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.".to_string(),
-        released: Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
-    };
-
     // -- add to collection
     // convert to bson instance
-    let serialized_movie = bson::to_bson(&parasite)?;
-    let document = serialized_movie.as_document().unwrap(); // safe to unwrap
+    let parasite = bson::to_bson(
+        &Movie {
+            id: None,
+            title: "Parasite".to_string(),
+            year: 2020,
+            plot: "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.".to_string(),
+            released: Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
+        }
+    )?;
+    let document = parasite.as_document().unwrap(); // safe to unwrap
 
     let movies = client.database("sample_mflix").collection("movies");
+
     let insert_result = movies.insert_one(document.clone(), None).await?;
-    let parasite_id = insert_result
+    let result_id = insert_result
         .inserted_id
         .as_object_id()
         .expect("Retrieved _id should have been of type ObjectId");
-    println!("Captain Marvel document ID: {:?}", parasite_id);
+    println!("Captain Marvel document ID: {:?}", result_id);
 
     // -- retrieve document
     let movie = movies
-        .find_one(Some(doc! { "_id":  parasite_id.clone() }), None)
+        .find_one(Some(doc! { "_id":  result_id.clone() }), None)
         .await?
         .expect("Document not found");
     let loaded_movie_struct = bson::from_bson::<Movie>(Bson::Document(movie))?;
