099ed752ec9b2cce5da07fa563d56fba8ec8479b
topheruk <kristopherab@gmail.com>
2022-03-08 01:15:39 +0000

    axum-demo with in memory repo works

--- examples/axum-demo/src/main.rs
+++ examples/axum-demo/src/main.rs
@@ -1,15 +1,81 @@
+use std::net::SocketAddr;
+
+use axum::{extract::Extension, routing::get, Router, Server};
+use handler::{todos_create, todos_delete, todos_index, todos_member, todos_update};
+use model::TodoRepo;
+
+pub mod error {
+    use axum::{
+        http::StatusCode,
+        response::{IntoResponse, Response},
+        Json,
+    };
+    use serde_json::json;
+
+    use crate::model::error::Error as ModelError;
+
+    pub enum Error {
+        Model(ModelError),
+    }
+
+    pub type Result<T> = std::result::Result<T, Error>;
+
+    impl From<ModelError> for Error {
+        fn from(e: ModelError) -> Self {
+            Self::Model(e)
+        }
+    }
+
+    impl IntoResponse for Error {
+        fn into_response(self) -> Response {
+            let (code, message) = match self {
+                Error::Model(ModelError::EmptyText) => {
+                    (StatusCode::BAD_REQUEST, "text field is emtpy".to_string())
+                }
+                Error::Model(ModelError::NotFound) => {
+                    (StatusCode::NOT_FOUND, "not found".to_string())
+                }
+                Error::Model(ModelError::Encoding(e)) => (StatusCode::BAD_REQUEST, e.to_string()),
+            };
+
+            let body = Json(json!({ "error": message }));
+
+            (code, body).into_response()
+        }
+    }
+}
+
 mod model {
     use std::{
         collections::HashMap,
         sync::{Arc, RwLock},
     };
 
+    use axum::async_trait;
     use serde::{Deserialize, Serialize};
     use uuid::Uuid;
 
-    pub type Database = Arc<RwLock<HashMap<Uuid, Todo>>>;
+    use self::error::Error;
 
-    #[derive(Serialize, Clone)]
+    pub mod error {
+        #[derive(Debug)]
+        pub enum Error {
+            Encoding(serde_json::Error),
+            // RwLockReadGuard Error
+            EmptyText,
+            NotFound,
+        }
+
+        impl From<serde_json::Error> for Error {
+            fn from(e: serde_json::Error) -> Self {
+                Self::Encoding(e)
+            }
+        }
+    }
+
+    type Result<T> = std::result::Result<T, Error>;
+
+    #[derive(Debug, Serialize, Clone)]
     pub struct Todo {
         pub id: Uuid,
         pub text: String,
@@ -17,119 +83,222 @@ mod model {
     }
 
     impl Todo {
-        pub fn new(text: String) -> Self {
+        // TODO:  what if empty string
+        // non public should prevent this being an issue
+        fn new(text: String) -> Self {
             Self {
                 id: Uuid::new_v4(),
                 text,
                 completed: false,
             }
         }
-    }
 
-    impl From<TodoDto> for Todo {
-        fn from(TodoDto { text }: TodoDto) -> Self {
-            Self::new(text)
+        pub fn default() -> Self {
+            Self::new("todo".to_string())
         }
     }
 
-    #[derive(Deserialize)]
-    pub struct TodoDto {
+    impl TryFrom<TodoCreate> for Todo {
+        type Error = Error;
+
+        fn try_from(value: TodoCreate) -> Result<Self> {
+            if value.text.is_empty() {
+                Err(Self::Error::EmptyText)
+            } else {
+                Ok(Self::new(value.text))
+            }
+        }
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct TodoUpdate {
+        pub text: Option<String>,
+        pub completed: Option<bool>,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct TodoCreate {
         pub text: String,
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct TodoRepo {
+        pub db: Arc<RwLock<HashMap<Uuid, Todo>>>,
+    }
+
+    impl TodoRepo {
+        pub fn new() -> Self {
+            Self { db: Arc::default() }
+        }
+    }
+
+    #[async_trait]
+    pub trait Repo {
+        async fn find_all(&self) -> Result<Vec<Todo>>;
+        async fn find(&self, id: Uuid) -> Result<Todo>;
+        async fn create(&mut self, dto: TodoCreate) -> Result<Uuid>;
+        async fn delete(&mut self, id: Uuid) -> Result<()>;
+        async fn update(&mut self, id: Uuid, dto: TodoUpdate) -> Result<Todo>;
+    }
+
+    #[async_trait]
+    impl Repo for TodoRepo {
+        async fn find(&self, id: Uuid) -> Result<Todo> {
+            let todos = self.db.read().unwrap(); // FIXME: say no to unwrap!!
+            let todo = todos.get(&id);
+
+            match todo {
+                Some(todo) => Ok(todo.clone()),
+                None => Err(Error::NotFound),
+            }
+        }
+
+        async fn find_all(&self) -> Result<Vec<Todo>> {
+            let todos = self.db.read().unwrap(); // FIXME: say no to unwrap!!
+            let todos = todos.values().cloned().collect::<Vec<_>>();
+
+            Ok(todos)
+        }
+
+        async fn create(&mut self, dto: TodoCreate) -> Result<Uuid> {
+            let todo: Todo = dto.try_into().unwrap(); // FIXME: say no to unwrap!!
+            let id = todo.id;
+
+            let mut todos = self.db.write().unwrap(); // FIXME: say no to unwrap!!
+            todos.insert(id, todo);
+
+            Ok(id)
+        }
+
+        async fn delete(&mut self, id: Uuid) -> Result<()> {
+            let mut todos = self.db.write().unwrap();
+
+            match todos.remove(&id).is_some() {
+                true => Ok(()),
+                false => Err(Error::NotFound),
+            }
+        }
+
+        async fn update(&mut self, id: Uuid, dto: TodoUpdate) -> Result<Todo> {
+            let mut todo = self.find(id).await?;
+
+            if let Some(text) = dto.text {
+                // TODO: I need to check if this catches EmtpyText too? Probs not
+                todo.text = text;
+            }
+
+            if let Some(completed) = dto.completed {
+                todo.completed = completed;
+            }
+
+            let mut todos = self.db.write().unwrap(); // FIXME: say no to unwrap!!
+
+            todos.insert(todo.id, todo.clone());
+
+            Ok(todo)
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        macro_rules! test_todo_create {
+            ($name:ident, $str:expr) => {
+                #[test]
+                fn $name() {
+                    let t = TodoCreate {
+                        text: $str.to_string(),
+                    };
+
+                    match t.try_into() as Result<Todo> {
+                        Ok(t) => assert_eq!(t.completed, false),
+                        Err(_) => assert!(true), // Err(e) => assert_eq!(e, error::Error::EmptyText),
+                    };
+                }
+            };
+        }
+
+        test_todo_create!(todo_create_todo_ok, "John");
+        test_todo_create!(test_create_todo_err, "");
+
+        #[test]
+        fn test_todo_json() -> Result<()> {
+            let t = serde_json::from_str::<TodoCreate>(r#"{"text":"Hello, World"}"#)?;
+            let t: Todo = t.try_into()?;
+            assert_eq!(t.completed, false);
+            Ok(())
+        }
     }
 }
 
 mod handler {
     use axum::{
-        extract::{Extension, Query},
+        extract::{Extension, Path},
         http::StatusCode,
-        response::IntoResponse,
         Json,
     };
-    use axum_extra::routing::TypedPath;
-    use serde::Deserialize;
+    use serde_json::{json, Value};
     use uuid::Uuid;
 
-    use crate::model::{Database, Todo, TodoDto};
+    use crate::model::{Repo, Todo, TodoCreate, TodoRepo, TodoUpdate};
 
-    #[derive(Deserialize, Default)]
-    pub struct Pagination {
-        pub offset: Option<usize>,
-        pub limit: Option<usize>,
+    use crate::error::Result;
+
+    pub async fn todos_index(Extension(repo): Extension<TodoRepo>) -> Result<Json<Vec<Todo>>> {
+        let todos = repo.find_all().await?;
+
+        Ok(Json(todos))
     }
 
-    #[derive(Deserialize, TypedPath)]
-    #[typed_path("/todos/")]
-    pub struct TodosCollection;
+    pub async fn todos_member(
+        Path(id): Path<Uuid>,
+        Extension(repo): Extension<TodoRepo>,
+    ) -> Result<Json<Todo>> {
+        let todo = repo.find(id).await?;
 
-    pub async fn todos_index(
-        _: TodosCollection,
-        Query(Pagination { offset, limit }): Query<Pagination>,
-        Extension(db): Extension<Database>,
-    ) -> impl IntoResponse {
-        let todos = db.read().unwrap();
-
-        let todos = todos
-            .values()
-            .skip(offset.unwrap_or(0))
-            .take(limit.unwrap_or(usize::MAX))
-            .cloned()
-            .collect::<Vec<_>>();
-
-        Json(todos)
+        Ok(Json(todo))
     }
 
     pub async fn todos_create(
-        _: TodosCollection,
-        Json(input): Json<TodoDto>,
-        Extension(db): Extension<Database>,
-    ) -> impl IntoResponse {
-        let todo: Todo = input.into();
+        Json(dto): Json<TodoCreate>,
+        Extension(mut repo): Extension<TodoRepo>,
+    ) -> Result<(StatusCode, Json<Value>)> {
+        let uid = repo.create(dto).await?;
 
-        db.write().unwrap().insert(todo.id, todo.clone());
-
-        (StatusCode::CREATED, Json(todo))
+        Ok((StatusCode::CREATED, Json(json!({ "id": uid }))))
     }
 
-    #[derive(Deserialize, TypedPath)]
-    #[typed_path("/todos/:id")]
-    pub struct TodosMember {
-        id: Uuid,
+    pub async fn todos_update(
+        Path(id): Path<Uuid>,
+        Json(dto): Json<TodoUpdate>,
+        Extension(mut repo): Extension<TodoRepo>,
+    ) -> Result<(StatusCode, Json<Todo>)> {
+        let todo = repo.update(id, dto).await?;
+
+        Ok((StatusCode::CREATED, Json(todo)))
     }
 
     pub async fn todos_delete(
-        TodosMember { id }: TodosMember,
-        Extension(db): Extension<Database>,
-    ) -> impl IntoResponse {
-        if let Some(_) = db.write().unwrap().remove(&id) {
-            StatusCode::NO_CONTENT
-        } else {
-            StatusCode::NOT_FOUND
-        }
+        Path(id): Path<Uuid>,
+        Extension(mut repo): Extension<TodoRepo>,
+    ) -> Result<StatusCode> {
+        repo.delete(id).await?;
+
+        Ok(StatusCode::NO_CONTENT)
     }
 }
 
-use std::net::SocketAddr;
-
-use axum::{extract::Extension, Router, Server};
-use axum_extra::routing::RouterExt;
-use handler::{todos_create, todos_delete, todos_index};
-use model::Database;
-use tower::ServiceBuilder;
-use tower_http::trace::TraceLayer;
-
-pub fn app() -> Router {
-    let db = Database::default();
+fn app() -> Router {
+    let repo = TodoRepo::new();
 
     Router::new()
-        .typed_get(todos_index)
-        .typed_post(todos_create)
-        .typed_delete(todos_delete)
-        .layer(
-            ServiceBuilder::new()
-                .layer(TraceLayer::new_for_http())
-                .layer(Extension(db))
-                .into_inner(),
+        .route("/todos/", get(todos_index).post(todos_create))
+        .route(
+            "/todos/:id",
+            get(todos_member).patch(todos_update).delete(todos_delete),
         )
+        .layer(Extension(repo))
 }
 
 #[tokio::main]
@@ -151,29 +320,28 @@ mod tests {
     use serde_json::json;
     use tower::ServiceExt; // for `app.oneshot()`
 
-    macro_rules! test_endpoint {
-        ($n:ident, $req:expr, $sc:expr) => {
+    macro_rules! test_post {
+        ($test_name:ident, $uri:expr, $json:expr, $status_code:expr) => {
             #[tokio::test]
-            async fn $n() {
+            async fn $test_name() {
                 let app = app();
-                let res = app.oneshot($req.unwrap()).await.unwrap();
+                let req = Request::builder()
+                    .method(Method::POST)
+                    .uri($uri)
+                    .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(serde_json::to_vec(&$json).unwrap().into())
+                    .unwrap();
 
-                assert_eq!(res.status(), $sc);
+                let res = app.oneshot(req).await.unwrap();
+                assert_eq!(res.status(), $status_code);
             }
         };
     }
 
-    test_endpoint!(
-        case_1,
-        Request::builder()
-            .method(Method::POST)
-            .uri("/todos/")
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(
-                serde_json::to_vec(&json!({"text":"Hello, World"}))
-                    .unwrap()
-                    .into(),
-            ),
+    test_post!(
+        case_created,
+        "/todos/",
+        json!({"text":"Hello, World!"}),
         StatusCode::CREATED
     );
 }
